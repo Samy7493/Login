50d7cdb57c8af18f147ab58b0599230ecd684a4e
sakhe <sakhe@SAMY>
2025-08-26 15:56:34 -0400

    Ajoutez des fichiers projet.

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text)) { MessageBox.Show("Please enter your username!", "Missing Username", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); return; }
+            string password = textBox2.Text;
+            bool hasNumber = password.Any(char.IsDigit);
+
+            if (password.Length < 4 || !hasNumber)
+            {
+                MessageBox.Show("Wrong password!", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // Proceed with login logic here
+            MessageBox.Show("Login successful!", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private Button loginButton;
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label5 = new Label();
+            loginButton = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(23, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(651, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Welcome to the app! Please sign in to continue.";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(23, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(78, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Username:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(107, 105);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(279, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(23, 172);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 20);
+            label3.TabIndex = 3;
+            label3.Text = "Password:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(107, 165);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(279, 27);
+            textBox2.TabIndex = 4;
+            textBox2.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(23, 222);
+            label5.Name = "label5";
+            label5.RightToLeft = RightToLeft.No;
+            label5.Size = new Size(684, 20);
+            label5.TabIndex = 6;
+            label5.Text = "Password requirements: Passwords Must Contain at least 4 characters and They Must contain a number.";
+            // 
+            // loginButton
+            // 
+            loginButton.Location = new Point(23, 267);
+            loginButton.Name = "loginButton";
+            loginButton.Size = new Size(100, 30);
+            loginButton.TabIndex = 0;
+            loginButton.Text = "Login";
+            loginButton.Click += loginButton_Click;
+            // 
+            // Form1
+            // 
+            AcceptButton = loginButton;
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(725, 336);
+            Controls.Add(loginButton);
+            Controls.Add(label5);
+            Controls.Add(textBox2);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label5;
+    }
+}
